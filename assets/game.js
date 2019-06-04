fb80b9ef84ba18f4991934de275cc628bc632125
Amanda Lowell <amandalowell2016@gmail.com>
2019-06-03 20:07:32 -0500

    final

--- assets/game.js
+++ assets/game.js
@@ -30,7 +30,7 @@ function startGame(){
     totalScore = 0;
     pink = generateRandomNumber (1,12);
     blue = generateRandomNumber(1,12);
-    yellow = generateRandomNumber(1,12);
+    yellow = generateRandomNumber(1,12); 
     green= generateRandomNumber(1,12);
     //set all crystals^
 }
@@ -38,24 +38,51 @@ function startGame(){
 
 $("#pinkCrystal").click(function(){
     totalScore = totalScore + pink;
+    checkWin();
+    
 })
 
 $("#blueCrystal").click(function(){
     totalScore = totalScore + blue;
+    checkWin();
+    
 })
 
 $("#yellowCrystal").click(function(){
     totalScore = totalScore + yellow;
+    checkWin();
+    
 })
 
-$("greenCrystal").click(function(){
+$("#greenCrystal").click(function(){
     totalScore = totalScore + green;
+    checkWin();
+    
 })
 
-
+startGame(); 
+checkWin();
+console.log("pink", pink,"blue", blue ,"yellow", yellow, "green", green); //
 
 // function start / restart game, all numbers set to 0, reset numbers
-//functions for when each crystal is cliked add "this " random numeber diff functions for each crystal
-//funtion to check if game is over 
-//function for if total score=random number, win ++, restart game
-//call start game after a win to restart game to get random number
+function checkWin(){
+    if (totalScore === randomNumber) { //means they won
+        wins++; 
+        startGame();  //adds point to wins
+    } else if (totalScore > randomNumber) { //means they loss
+        losses++; //adds point to losses
+        startGame();
+    }
+    $("#totalScore").text(totalScore);
+    //update items on screen, wins, losses, randomNumber , match # with var
+
+    $("#wins").text(wins);
+    $("#losses").text(losses);
+    $("#randomNumber").text(randomNumber);
+}
+
+
+
+
+
+
